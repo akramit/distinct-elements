9f23244e483c1a032e1fbdc86659a28e9e263d7e
Amit Roy <amit.akr.roy@gmail.com>
2025-03-28 18:49:28 +0100

    minor

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ def distinct_elements(eps,delta,arr):
     for i in range(0,len(arr)):
         distinct_elements.remove(arr[i])
         # with probability p, remove arr[i]
-
+        if insert_with_prob(p) :
+            distinct_elements.insert(arr[i])
+        if len(distinct_elements) >= thresh:
+            distinct_elements = distinct_elements[:thresh]
 
 
 
