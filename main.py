09683f3d4064e0905f2c219d0ea793389b7c78b7
Amit Roy <amit.akr.roy@gmail.com>
2025-03-29 13:31:39 +0100

    algorithm completed

--- main.py
+++ main.py
@@ -1,19 +1,54 @@
 import math
+import random
 
+def insert_with_prob(p): 
+    rand_len = math.ceil(1/p)
+    rand_num = random.randint(1,rand_len)
+    if rand_num == 1:
+        return True
+    return False
+
+def throw_elements_with_prob(elements, p):
+    remove_elements_index = []
+    for i in range(0, len(elements)):
+        curr_el = elements[i]
+        rand_len = math.ceil(1/p)
+        rand_num = random.randint(1,rand_len)
+        if rand_num == 1:
+            remove_elements_index.append(i)
+
+    final_elements = [] 
+    for i in range(0, len(elements)):
+        if i not in remove_elements_index:
+            final_elements.append(elements[i])
+    return final_elements
 
 def distinct_elements(eps,delta,arr): 
     p=1
-    thresh = 1/(eps**2)*12*log(len(arr)/delta)
+    thresh = 1/(eps**2)*12*math.log(len(arr)/delta)
+    print("Threshold: ",thresh)
     distinct_elements = []
     for i in range(0,len(arr)):
-        distinct_elements.remove(arr[i])
+        if arr[i] in distinct_elements:
+            distinct_elements.remove(arr[i])
         # with probability p, remove arr[i]
         if insert_with_prob(p) :
-            distinct_elements.insert(arr[i])
+            distinct_elements.append(arr[i])
         if len(distinct_elements) >= thresh:
-            distinct_elements = distinct_elements[:thresh]
-
+            #throw each element in distinct_elements with prob p
+            distinct_elements = throw_elements_with_prob(distinct_elements, p)
+            p = p/2
+            if len(distinct_elements) == thresh:
+                print("Algorithm Failed")
+                return
+    return len(distinct_elements)//p        
 
 
 if __name__ == "__main__":
-    distinct_elements()
+    eps = 0.1
+    delta = 0.1
+    arr = [random.randint(1000000,9000000) for i in range(0,1000000)]
+    set_arr = set(arr)
+    print("Acutal Distinct Elements: ", len(set_arr))
+    output = distinct_elements(eps,delta, arr)
+    print("Algorithm Distinct Elements: ", output)
